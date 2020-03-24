eca51ab6ea40338563a0f846326c50ef40d9572d
taoLiu1998 <nicai8520>
2020-03-25 06:47:55 +0800

    第一次使用git提交到本地仓库

---
+++ src/main/java/com/hr/gittext/GittextApplication.java
@@ -0,0 +1,13 @@
+package com.hr.gittext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GittextApplication.class, args);
+    }
+
+}
